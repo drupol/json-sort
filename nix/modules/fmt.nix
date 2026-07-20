b050f47d39a0d252e1e63ede85c66aca8a08036c
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-07-20 12:39:30 +0200

    feat: add, use and expose a `treefmt` module

--- nix/modules/fmt.nix
+++ nix/modules/fmt.nix
@@ -1,12 +1,18 @@
 { inputs, ... }:
 {
-  imports = [ inputs.treefmt-nix.flakeModule ];
+  imports = [
+    inputs.treefmt-nix.flakeModule
+  ];
 
   perSystem = {
     treefmt = {
+      imports = [
+        inputs.self.treefmtModules.default
+      ];
       projectRootFile = "flake.nix";
       programs = {
         jsonfmt.enable = true;
+        json-sort.enable = true;
         nixfmt.enable = true;
         prettier.enable = true;
         statix.enable = true;
